a60800ab101378bf83e0b0c0b037a749c93ba6ed
linsera1809 <blinse9@yahoo.com>
2020-10-08 02:20:54 -0400

    build script is a work in progress

---
+++ build.py
@@ -0,0 +1,8 @@
+import src
+# What files we are importing from project:
+from src import App
+from src import API_Controller
+from src import GUI_Controller
+
+if __name__ == '__main__':
+    App.main()
